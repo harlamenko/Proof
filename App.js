23e05ee557ac94fb8211bbc19d3b3b98a12789ce
Artem Harlamenko <4.harlamenko@gmail.com>
2020-02-07 20:51:46 +0300

    add base navigation

--- App.js
+++ App.js
@@ -1,9 +1,30 @@
+import 'react-native-gesture-handler';
 import React from 'react';
-import { AppContainer } from './navigation';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import { Welcome } from './screens/Welcome';
+import { ProductDetails } from './screens/ProductDetails';
+import { Products } from './screens/Products';
+
+const Stack = createStackNavigator();
+
 export default class App extends React.Component {
   render( ){
     return (
-      <AppContainer/>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Welcome">
+          <Stack.Screen
+            name='Welcome'
+            component={Welcome}
+            options={{
+              headerShown: false
+            }}
+          />
+          <Stack.Screen name='ProductDetails' component={ProductDetails} />
+          <Stack.Screen name='Products' component={Products} />
+        </Stack.Navigator>
+      </NavigationContainer>
     );
   }
 }
