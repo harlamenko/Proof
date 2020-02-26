a592aaa112447b6a164e191c0295847475840d3d
Artem Harlamenko <4.harlamenko@gmail.com>
2020-02-26 12:08:24 +0300

    change headers

--- App.js
+++ App.js
@@ -2,12 +2,11 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-
+import { Header } from 'react-native-elements';
 import { Welcome } from './screens/Welcome';
 import { AdvertDetails } from './screens/AdvertDetails';
 import { Adverts } from './screens/Adverts';
 import { AddAdvert } from './screens/AddAdvert';
-
 
 const Stack = createStackNavigator();
 
@@ -28,22 +27,57 @@ export default class App extends React.Component {
             component={Adverts}
             options={{
               headerTitle: 'Объявления',
-              headerStatusBarHeight: 20
+              header: props => <Header
+                leftComponent={{
+                  icon: 'arrow-back',
+                  color: '#fff',
+                  onPress: () => props.navigation.goBack()
+                }}
+                centerComponent={{
+                  text: 'Объявления',
+                  style: { color: '#fff' }
+                }}
+                rightComponent={{
+                  icon: 'add',
+                  color: '#fff',
+                  onPress: () => props.navigation.navigate('AddAdvert')
+                }}
+              />
             }}
           />
           <Stack.Screen
             name='AdvertDetails'
             component={AdvertDetails}
             options={{
-              // TODO: добавить название объявления
-              headerStatusBarHeight: 20
+              header: props => <Header
+                leftComponent={{
+                  icon: 'arrow-back',
+                  color: '#fff',
+                  onPress: () => props.navigation.goBack()
+                }}
+                centerComponent={{
+                  // TODO: добавить название объявления
+                  text: 'Объявление',
+                  style: { color: '#fff' }
+                }}
+              />
             }}
           />
           <Stack.Screen
             name='AddAdvert'
             component={AddAdvert}
             options={{
-              headerTitle: 'Добавление объявления'
+              header: props => <Header
+                leftComponent={{
+                  icon: 'arrow-back',
+                  color: '#fff',
+                  onPress: () => props.navigation.goBack()
+                }}
+                centerComponent={{
+                  text: 'Добавление объявления',
+                  style: { color: '#fff' }
+                }}
+              />
             }}
           />
         </Stack.Navigator>
