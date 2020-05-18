8e2a0c1623040ef0bc533eb2df663c9b3e9f0d92
Artem Harlamenko <4.harlamenko@gmail.com>
2020-05-18 23:36:20 +0300

    feature: initial chat UI

--- mobile/screens/chat/Chat.js
+++ mobile/screens/chat/Chat.js
@@ -1,13 +1,33 @@
 import React from 'react';
 import { Text } from 'react-native-elements';
+import { GiftedChat } from 'react-native-gifted-chat'
+import { View } from 'react-native';
+import { Layout } from '../../shared/styles';
 
 export default class Chat extends React.Component {
+    state = {
+        messages: [],
+    }
+
     constructor(props) { super(props); }
 
     componentDidMount() {
-        this.props.navigation.dangerouslyGetParent().setOptions({
-            headerShown: false,
-        });
+        this._hideHeader();
+
+        this.setState({
+            messages: [
+                {
+                    _id: 1,
+                    text: 'Hello developer',
+                    createdAt: new Date(),
+                    user: {
+                        _id: 2,
+                        name: 'React Native',
+                        avatar: 'https://placeimg.com/140/140/any',
+                    },
+                },
+            ],
+        })
     }
 
     componentWillUnmount() {
@@ -16,10 +36,36 @@ export default class Chat extends React.Component {
         });
     }
 
-    render() {
+    onSend(messages = []) {
+        this.setState(previousState => ({
+            messages: GiftedChat.append(previousState.messages, messages),
+        }))
+    }
+
+    _hideHeader = () => {
         this.props.navigation.dangerouslyGetParent().setOptions({
             headerShown: false,
         });
-        return <Text>Chat</Text>
     }
+
+    render() {
+        this._hideHeader();
+
+        return (
+            <View style={Layout.centeringContainer}>
+                <Text h4>Список диалогов пуст.</Text>
+            </View>
+        )
+
+        return (
+            <GiftedChat
+                messages={this.state.messages}
+                onSend={messages => this.onSend(messages)}
+                user={{
+                    _id: 1,
+                }}
+            />
+        )
+    }
+
 }
