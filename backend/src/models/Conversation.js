77b0f562e93eb9ecb9986c809c3c314ebd71f4c4
Artem Harlamenko <4.harlamenko@gmail.com>
2020-05-30 17:57:32 +0300

    feature: chat models extended

--- backend/src/models/Conversation.js
+++ backend/src/models/Conversation.js
@@ -1,17 +1,19 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const conversationSchema = new Schema({
-    /** Продавец устройства */
-    seller: { type: Schema.Types.ObjectId, ref: 'User' },
-    /** Покупатель устройства */
-    buyer: { type: Schema.Types.ObjectId, ref: 'User' },
-    /** Устройство, по которому ведется диалог */
-    advert: { type: Schema.Types.ObjectId, ref: 'Advert' },
-    /** Текст последнего сообщения */
-    last_message: { type: String },
-    /** Дата обновления */
-    updated_at: { type: Date, default: Date.now },
+  /** Продавец устройства */
+  seller: { type: Schema.Types.ObjectId, ref: "User" },
+  /** Покупатель устройства */
+  buyer: { type: Schema.Types.ObjectId, ref: "User" },
+  /** Устройство, по которому ведется диалог */
+  advert: { type: Schema.Types.ObjectId, ref: "Advert" },
+  /** Текст последнего сообщения */
+  last_message: { type: String },
+  /** Дата обновления */
+  updated_at: { type: Date, default: Date.now },
+  /** Кол-во непрочитанных сообщений в диалоге */
+  unread_count: { type: Number, default: 0, min: 0 },
 });
 
-mongoose.model('Conversation', conversationSchema);
+mongoose.model("Conversation", conversationSchema);
--- backend/src/models/Message.js
+++ backend/src/models/Message.js
@@ -1,17 +1,19 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const messageSchema = new Schema({
-    /** Идентификатор отправителя */
-    user: { type: Schema.Types.ObjectId, ref: 'User' },
-    /** Идентификатор диалога */
-    conversation: { type: Schema.Types.ObjectId, ref: 'Conversation' },
-    /** Текст сообщения */
-    text: { type: String, maxlength: 720 },
-    /** Изображение */
-    image: { type: String },
-    /** Дата отправки */
-    createdAt: { type: Date, default: Date.now },
+  /** Идентификатор отправителя */
+  user: { type: Schema.Types.ObjectId, ref: "User" },
+  /** Идентификатор диалога */
+  conversation: { type: Schema.Types.ObjectId, ref: "Conversation" },
+  /** Текст сообщения */
+  text: { type: String, maxlength: 720 },
+  /** Изображение */
+  image: { type: String },
+  /** Дата отправки */
+  createdAt: { type: Date, default: Date.now },
+  /** Прочитано ли сообщение */
+  read: { type: Boolean, default: false },
 });
 
-mongoose.model('Message', messageSchema);
+mongoose.model("Message", messageSchema);
