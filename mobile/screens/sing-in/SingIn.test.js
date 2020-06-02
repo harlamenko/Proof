57270140245cecf939a61314c6529463834897c9
Artem Harlamenko <4.harlamenko@gmail.com>
2020-06-02 20:58:49 +0300

    feature: sinhin tests

--- mobile/screens/sing-in/SingIn.test.js
+++ mobile/screens/sing-in/SingIn.test.js
@@ -1,1 +1,24 @@
-describe('SingIn screen', () => {});
+jest.mock('react-native-simple-toast', () => ({
+  SHORT: jest.fn(),
+}));
+import React from 'react';
+import { render } from 'react-native-testing-library';
+import { AuthProvider } from '../../context';
+import SignIn from './SingIn';
+
+const TestComponent = () => (
+  <AuthProvider>
+    <SignIn />
+  </AuthProvider>
+);
+
+describe('SingIn screen', () => {
+  it('should render', () => {
+    expect(render(<TestComponent />)).not.toBeNull();
+  });
+
+  it('should render correctly', () => {
+    const SignInScreen = render(<TestComponent />).toJSON();
+    expect(SignInScreen).toMatchSnapshot();
+  });
+});
