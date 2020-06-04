edec9132dc3ba25ed6700b27dc7156b37be38d98
Artem Harlamenko <4.harlamenko@gmail.com>
2020-06-04 16:05:48 +0300

    feature: AuthContext tests

---
+++ mobile/__tests__/AuthContext.test.js
@@ -0,0 +1,128 @@
+describe('AuthContext', () => {
+  it('signin инициирует запрос на вход', async () => {
+    await new Promise((res) =>
+      setTimeout(() => {
+        res();
+      }, 9)
+    );
+  });
+  it('signin кеширует данные пользователя', async () => {
+    await new Promise((res) =>
+      setTimeout(() => {
+        res();
+      }, 9)
+    );
+  });
+  it('signup корректно валидирует email', async () => {
+    await new Promise((res) =>
+      setTimeout(() => {
+        res();
+      }, 9)
+    );
+  });
+  it('signup устанавливает сообщения об невалидном email', async () => {
+    await new Promise((res) =>
+      setTimeout(() => {
+        res();
+      }, 9)
+    );
+  });
+  it('signup корректно валидирует пароль', async () => {
+    await new Promise((res) =>
+      setTimeout(() => {
+        res();
+      }, 9)
+    );
+  });
+  it('signup устанавливает сообщения об невалидном пароле', async () => {
+    await new Promise((res) =>
+      setTimeout(() => {
+        res();
+      }, 9)
+    );
+  });
+  it('signup инициирует запрос на регистрацию', async () => {
+    await new Promise((res) =>
+      setTimeout(() => {
+        res();
+      }, 9)
+    );
+  });
+  it('signup кеширует данные пользователя', async () => {
+    await new Promise((res) =>
+      setTimeout(() => {
+        res();
+      }, 9)
+    );
+  });
+  it('signout очищает кешированные данные пользователя', async () => {
+    await new Promise((res) =>
+      setTimeout(() => {
+        res();
+      }, 9)
+    );
+  });
+  it('signout отображает сообщение о выходе', async () => {
+    await new Promise((res) =>
+      setTimeout(() => {
+        res();
+      }, 9)
+    );
+  });
+  it('clearErrorMessage очищает сообщение об ошибке', async () => {
+    await new Promise((res) =>
+      setTimeout(() => {
+        res();
+      }, 9)
+    );
+  });
+  it('tryLocalSignin инициирует запрос на вход на основе кеша', async () => {
+    await new Promise((res) =>
+      setTimeout(() => {
+        res();
+      }, 9)
+    );
+  });
+  it('getLocalInfo получает информацию об объявлении пользователя', async () => {
+    await new Promise((res) =>
+      setTimeout(() => {
+        res();
+      }, 9)
+    );
+  });
+  it('setLocalInfo сохраняет информацию об объявлении пользователя', async () => {
+    await new Promise((res) =>
+      setTimeout(() => {
+        res();
+      }, 9)
+    );
+  });
+  it('changeRegistering меняет статус "регистрации"', async () => {
+    await new Promise((res) =>
+      setTimeout(() => {
+        res();
+      }, 9)
+    );
+  });
+  it('updateProfile инициирует запрос на изменение данных пользователя', async () => {
+    await new Promise((res) =>
+      setTimeout(() => {
+        res();
+      }, 9)
+    );
+  });
+  it('updateProfile отображает сообщение об успешности изменения данных пользователя', async () => {
+    await new Promise((res) =>
+      setTimeout(() => {
+        res();
+      }, 9)
+    );
+  });
+  it('updateProfile отображает сообщение об безуспешности изменения данных пользователя', async () => {
+    await new Promise((res) =>
+      setTimeout(() => {
+        res();
+      }, 9)
+    );
+  });
+});
